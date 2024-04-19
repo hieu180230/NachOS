94007c2698786aeff5f44d38881eb9be928fc8ec
KV <vienguyenkhac224@gmail.com>
2024-04-19 10:20:21 +0700

    a,b,c

--- nachos-3.4/code/threads/system.h
+++ nachos-3.4/code/threads/system.h
@@ -34,6 +34,11 @@ extern Timer *timer;				// the hardware alarm clock
 #include "synchcons.h"
 extern Machine* machine;	// user program memory and registers
 extern SynchConsole* gSynchConsole;
+
+extern Semaphore *addrLock;	// semaphore
+extern BitMap *gPhysPageBitMap;	// quan ly cac frame
+extern PTable *pTab;		// quan ly bang tien trinh
+extern STable *semTab;		// quan ly semaphore
 #endif
 
 #ifdef FILESYS_NEEDED 		// FILESYS or FILESYS_STUB 
