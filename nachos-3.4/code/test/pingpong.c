44182ebeece6de62928eab6e38731a48694ea029
Cookie <hieu180230@gmail.com>
2024-04-21 08:38:18 +0700

    done pingpong

---
+++ nachos-3.4/code/test/pingpong.c
@@ -0,0 +1,14 @@
+#include "syscall.h"
+#include "copyright.h"
+
+int main()
+{
+	int PingID, PongID;
+	PrintString("PingPong Test Starting... \n");
+	PingID = Exec("./test/a");
+	PongID = Exec("./test.b");
+	Join(PingID);
+	Join(PongID);
+	return 0;
+}
+
