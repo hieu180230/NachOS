8e6c7d546ea567ee2ceda7049c315902847cc82f
Cookie <hieu180230@gmail.com>
2024-04-21 09:08:13 +0700

    done real

--- nachos-3.4/code/test/pingpong.c
+++ nachos-3.4/code/test/pingpong.c
@@ -6,7 +6,7 @@ int main()
 	int PingID, PongID;
 	PrintString("PingPong Test Starting... \n");
 	PingID = Exec("./test/a");
-	PongID = Exec("./test.b");
+	PongID = Exec("./test/b");
 	Join(PingID);
 	Join(PongID);
 	return 0;
